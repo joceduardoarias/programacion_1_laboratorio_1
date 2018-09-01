8c59670802e48558bba30e5b526f79773dbcf7c7
n-alegria <n-alegria@outlook.com>
2018-09-01 18:55:12 -0300

    Trabajo practico n°1

--- Trabajos Practicos/TP1/Trabajo Practico Nº1/main.c
+++ Trabajos Practicos/TP1/Trabajo Practico Nº1/main.c
@@ -11,12 +11,14 @@ int main()
     int opcion;
     char seguir='s';
 
+    int flag = 0;
     float suma;
     float resta;
     float division;
     float multiplicacion;
 
     while(seguir == 's'){
+        system("cls");
         printf("1. Ingresar 1er operando (A=%.2f)\n", primerOperando);
         printf("2. Ingresar 2do operando (B=%.2f)\n", segundoOperando);
         printf("3. Calcular todas las operaciones\n");
@@ -26,11 +28,6 @@ int main()
         printf("\td) Calcular la multiplicacion (A*B)\n");
         printf("\te) Calcular el factorial (A!)\n");
         printf("4. Informar resultados\n");
-        printf("\ta) El resultado de A+B es: r\n");
-        printf("\tb) El resultado de A-B es: r\n");
-        printf("\tc) El resultado de A/B es: r” o “No es posible dividir por cero”\n");
-        printf("\td) El resultado de A*B es: r”\n");
-        printf("\te) El factorial de A es: r1 y El factorial de B es: r2”\n");
         printf("5. Salir\n\n");
         printf("Ingrese una opcion: ");
         opcion = getche();
@@ -47,10 +44,32 @@ int main()
                 system("cls");
                 break;
             case '3':
+                system("cls");
+                printf("Se realizaron las operaciones solicitadas.\n\n");
                 suma = primerOperando + segundoOperando;
                 resta = primerOperando - segundoOperando;
                 multiplicacion = primerOperando * segundoOperando;
                 division = primerOperando / segundoOperando;
+                flag = 1;
+                system("pause");
+                break;
+            case '4':
+                system("cls");
+                if(primerOperando == 0 && segundoOperando == 0){
+                    printf("Aun no se realiazaron operaciones.\n\n");
+                }
+                else{
+                    printf("Resultados\n\n");
+                    printf("\ta) El resultado de %.2f+%.2f es: %.2f\n", primerOperando, segundoOperando, suma);
+                    printf("\tb) El resultado de %.2f-%.2f es: %.2f\n", primerOperando, segundoOperando, resta);
+                    printf("\tc) El resultado de %.2f/%.2f es: %.2f o No es posible dividir por cero\n", primerOperando, segundoOperando, division);
+                    printf("\td) El resultado de %.2f*%.2f es: %.2f\n", primerOperando, segundoOperando, multiplicacion);
+                    printf("\te) El factorial de A es: r1 y El factorial de B es: r2\n\n");
+                }
+                system("pause");
+                break;
+            case '5':
+                seguir = 'n';
                 break;
         }
 
