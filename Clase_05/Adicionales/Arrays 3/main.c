c08ffb8f4aeb88e17e768739149db9d62ac10748
n-alegria <n.alegria@outlook.com>
2018-09-07 21:54:25 -0300

    Ejercicios adicionales Arrays

--- Clase_05/Adicionales/Arrays 3/main.c
+++ Clase_05/Adicionales/Arrays 3/main.c
@@ -2,16 +2,33 @@
 #include <stdlib.h>
 #define TAM 5
 
+int buscarNumero(int x[], int tam, int valor);
 int buscarMayor(int x[], int tam);
+void mostrarVector(int x[], int tam);
 
 int main()
 {
     int valor;
-    int vector [TAM] = {5,100,9,30,2};
+    int indice;
+    int vector [TAM] = {5,10,9,300,2};
     valor = buscarMayor(vector, TAM);
-    printf("El numero mayor es: %d\n\n", valor);
+    indice = buscarNumero(vector, TAM, valor);
+    mostrarVector(vector, TAM);
+    printf("\n\nEl numero mayor es: %d y esta en el indice %d\n\n", valor, indice);
     return 0;
 }
+
+int buscarNumero(int x[], int tam, int valor)
+{
+    int indice = -1;
+    for(int i = 0; i<tam; i++){
+        if(valor == x[i]){
+            indice=i;
+        }
+    }
+    return indice;
+}
+
 
 int buscarMayor(int x[], int tam)
 {
@@ -22,4 +39,13 @@ int buscarMayor(int x[], int tam)
         }
     }
     return mayor;
+}
+
+void mostrarVector(int x[], int tam)
+{
+    printf("Vector: ");
+    for(int i=0; i<tam; i++)
+    {
+        printf("%d ", x[i]);
+    }
 }
