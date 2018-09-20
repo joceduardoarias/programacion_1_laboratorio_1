9610c7cd403831fa8027bb3cd4f4d779dadc72e6
Néstor Alegría <36082853+n-alegria@users.noreply.github.com>
2018-09-20 01:32:17 -0300

    Update Alegria.c

--- Bibliotecas/Validaciones/Validacion Entero/Alegria.c
+++ Bibliotecas/Validaciones/Validacion Entero/Alegria.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 #include "Alegria.h"
 
-int validarRango(char mensaje[], char mensajeError[], int limInf, int limSup){
-    int edad;
+int validarRango(char mensaje[], char mensajeError[], int limiteInferior, int limiteSuperior){
+    int retorno = -1;
     printf("%s", mensaje);
     scanf("%d", &edad);
     while(edad<limInf || edad>limSup){
         printf("\n%s", mensajeError);
         printf("\n\n%s", mensaje);
         scanf("%d", &edad);
+        retorno = 0;
     }
-    return edad;
+    return retorno;
 }
 
