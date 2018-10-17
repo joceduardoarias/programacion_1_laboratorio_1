5eb62f7b393e4544107f816c83115e122681e028
n-alegria <n.alegria@outlook.com>
2018-10-17 11:01:03 -0300

    Punteros

--- Clase_15/Adicionales/Punteros/main.c
+++ Clase_15/Adicionales/Punteros/main.c
@@ -3,6 +3,7 @@
 
 int main()
 {
+    printf("Punteros inicializar y asignar  -->\n\n");
     int num = 5; //declaro variable int
     int *ptr; // declaro puntero ptr
     ptr = &num; //asigno direccion de memoria de num a ptr
@@ -20,11 +21,13 @@ int main()
     b = 15; // Asignamos el valor 15 a la variable 'b'
     a = &b; // Obtenemos la posición de memoria de 'b' con el operador '&'
     c = *a; // Asignamos el valor 15 a la variable 'c' por indireccion con el operador '*'
-    printf("Num c: %d", c);
-
+    printf("Valor B: %d", b);
+    printf("\nValor A: %d", *a);
+    printf("\nNum c: %d", c);
+    printf("\n<------------------");
     printf("\n\n\n");
 
-    printf("\nComparacion entre punteros: \n");
+    printf("Comparacion entre punteros: -->\n");
     int* punteroA;
     int* punteroB;
     int auxiliarC, auxiliarD;
@@ -41,16 +44,54 @@ int main()
     if(punteroA > punteroB);
     if(punteroA == 0XAAFF); //COMPARO PUNTERO CON UNA DIRECCION DE MEMORIA
     */
-
+    printf("\n<------------------");
     printf("\n\n\n");
 
-    printf("Operador binario 'sizeof'\n\n");
+    printf("Operador binario 'sizeof' -->\n\n");
     printf("Tamanio de char: %d",  sizeof(char));
     printf("\nTamanio de int: %d",  sizeof(int));
+    printf("\n<------------------");
 
     printf("\n\n\n");
 
+    printf("Vectores y punteros -->\n\n");
+    int vec[5];
+    int *p;
+    p = vec;
+    printf("Asigno con Vector.\n");
+    for(int i=0; i<5; i++){//asigno
+        vec[i] = i+3;
+    }
+    for(int i=0; i<5; i++){//imprimo
+        printf("%d ", vec[i]);
+    }
+    printf("\n\nAsigno con Puntero.\n");
+    for(int i=0; i<5; i++){//asigno
+        *(p+i) = i+5;
+    }
+    for(int i=0; i<5; i++){//imprimo
+        printf("%d ", *(p+i));
+    }
+    printf("\n<------------------");
 
+    printf("\n\n\n");
 
+    printf("Puntero a puntero -->\n\n");
+    int j=5;
+    int *punteroInt;
+    int **punteroPuntero;
+    punteroInt = &j; // Obtenemos la posición de memoria
+    punteroPuntero = &punteroInt; // Obtenemos la posición de memoria
+    printf("Direccion de memoria j: %p", &j);
+    printf("\nDato de j: %d", j);
+
+    printf("\n\nDireccion de memoria punteroInt: %p", punteroInt);
+    printf("\nDato de punteroInt: %d", *punteroInt);
+
+    printf("\n\nDireccion de memoria punteroPuntero: %p", punteroPuntero);
+    printf("\nDato de punteroPuntero: %d", *punteroPuntero);
+    printf("\n<------------------");
+
+    printf("\n\n");
     return 0;
 }
