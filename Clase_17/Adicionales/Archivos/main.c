382a8f8fa6d4d2de4a07dbec639161cab647e071
n-alegria <n.alegria@outlook.com>
2018-10-29 12:26:35 -0300

    Archivos

--- Clase_17/Adicionales/Archivos/main.c
+++ Clase_17/Adicionales/Archivos/main.c
@@ -1,34 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct{
+typedef struct
+{
     int legajo;
     char nombre[20];
     float sueldo;
-}eEmpleado;
+} eEmpleado;
 
-int parseData(FILE* archivo, eEmpleado* arrayEmpleados, int len);
+int parseData(char* archivo, eEmpleado* arrayEmpleados);
 
 int main()
 {
-    FILE* fl;
-    fl = fopen("empleados.csv", "r");
-    if(fl == NULL)
+    int r, i;
+    eEmpleado* pArrayEmpleados;
+
+    pArrayEmpleados = (eEmpleado*) malloc(sizeof(eEmpleado)*50);
+    if(pArrayEmpleados == NULL)
     {
-        printf("Error al cargar el archivo.\n");
+        printf("No hay lugar para crear array.\n");
         exit(EXIT_FAILURE);
     }
-    else{
-        printf("Archivo cargado con exito!\n");
+    printf("Array creado correctamente.\n");
+
+    r = parseData("empleados.csv", pArrayEmpleados);
+    for(i=0; i<r; i++)
+    {
+        printf("Legajo: %d   Nombre: %s   Sueldo: %.2f\n", pArrayEmpleados[i].legajo, pArrayEmpleados[i].nombre, pArrayEmpleados[i].sueldo);
     }
     return 0;
 }
 
-int parseData(FILE* archivo, eEmpleado* arrayEmpleados, int len)
+int parseData(char* archivo, eEmpleado* arrayEmpleados)
 {
+    FILE *pFile;
     int todoOk = 0;
+    int r,i=0;
+    char var1[50],var2[50],var3[50];
+    pFile = fopen(archivo,"r");
+    fscanf(pFile,"%[^,],%[^,],%[^\n]\n",var1,var2,var3);
 
+    if(pFile == NULL)
+    {
+        todoOk = 1;
+    }
+    do
+    {
+        r = fscanf(pFile,"%[^,],%[^,],%[^\n]\n",var1,var2,var3);
+        if(r==3)
+        {
+            arrayEmpleados[i].legajo = atoi(var1);
+            strcpy(arrayEmpleados[i].nombre,var2);
+            arrayEmpleados[i].sueldo = atof(var3);
 
+            i++;
+        }
+        else
+            break;
+    }
+    while(!feof(pFile));
+    fclose(pFile);
 
-    return todoOk;
+    return i;
 }
